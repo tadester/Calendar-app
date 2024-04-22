fdc8763ba7648dd17067725651f7a85298ce3ee4
tadester <obasantade@gmail.com>
2024-04-22 11:39:07 -0600

    Creating a database using mongoose to store refresh tokens and user information

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ const { google } = require('googleapis');
 const session = require('express-session');
 const calendarAPI = require('./calendar');
 require('dotenv').config({ path: './cal.env'});
+const helmet = require('helmet');
+app.use(helmet());
+const rateLimit = require('express-rate-limit');
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+});
 
+//  apply to all requests
+app.use(limiter);
 const app = express();
 const PORT = process.env.PORT;
 
@@ -18,7 +27,19 @@ const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_SECRET,
   process.env.REDIRECT_URI
 );
-
+// Automatically refresh and store tokens when they expire
+oauth2Client.on('tokens', (tokens) => {
+    if (tokens.refresh_token) {
+      // store the refresh_token in your database!
+      console.log(tokens.refresh_token);
+    }
+    console.log(tokens.access_token);
+  });
+  
+  // Set credentials including refresh token
+  oauth2Client.setCredentials({
+    refresh_token: "STORED_REFRESH_TOKEN"
+  });
 // Google's OAuth2 URL
 app.get('/auth/google', (req, res) => {
   const url = oauth2Client.generateAuthUrl({
---
+++ userservice.js
@@ -0,0 +1,30 @@
+const User = require('./UserModel');
+
+const storeOrUpdateUser = async (email, refreshToken) => {
+  try {
+    const updatedUser = await User.findOneAndUpdate(
+      { email: email },
+      { refreshToken: refreshToken },
+      { new: true, upsert: true }
+    );
+    console.log('User updated or inserted:', updatedUser);
+    return updatedUser;
+  } catch (err) {
+    console.error('Error updating or inserting user:', err);
+  }
+};
+
+const getRefreshToken = async (email) => {
+  try {
+    const user = await User.findOne({ email: email });
+    return user ? user.refreshToken : null;
+  } catch (err) {
+    console.error('Error retrieving user refresh token:', err);
+    return null;
+  }
+};
+
+module.exports = {
+  storeOrUpdateUser,
+  getRefreshToken
+};
